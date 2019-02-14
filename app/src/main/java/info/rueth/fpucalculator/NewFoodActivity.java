afe2d2c5633c4fea12630c18cee75dfbc293dbf4
UlricusR <ulrich.rueth@gmail.com>
2019-02-14 11:10:24 +0100

    New layout

--- app/src/main/java/info/rueth/fpucalculator/NewFoodActivity.java
+++ app/src/main/java/info/rueth/fpucalculator/NewFoodActivity.java
@@ -1,12 +1,15 @@
 package info.rueth.fpucalculator;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class NewFoodActivity extends AppCompatActivity {
@@ -17,7 +20,7 @@ public class NewFoodActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY_CARBS = "info.rueth.fpucalculator.foodlistsql.REPLY_CARBS";
 
     private EditText mFoodName;
-    private ToggleButton mFavorite;
+    private Switch mFavorite;
     private EditText mCalories;
     private EditText mCarbs;
 
@@ -47,6 +50,26 @@ public class NewFoodActivity extends AppCompatActivity {
                     boolean favorite = mFavorite.isChecked();
                     double calories = Double.parseDouble(mCalories.getText().toString());
                     double carbs = Double.parseDouble(mCarbs.getText().toString());
+
+                    // Check if all numbers are positive
+                    if (calories < 0 || carbs < 0) { // TODO Localize error message
+                        String errMsg = "Calories as well as carbs must not be negative for food " + foodName;
+                        Snackbar.make(findViewById(R.id.new_food),
+                                errMsg, Snackbar.LENGTH_LONG).show();
+                        setResult(RESULT_CANCELED, replyIntent);
+                    }
+
+                    // Do a consistency check: Calories from carbs cannot be more than
+                    // total calories; 1g of carbs has 4 kcal
+                    if (carbs * 4 > calories) { // TODO Localize error message
+                        String errMsg = "Cannot construct new food " + foodName
+                                + ": Calories from carbs (1g ~ 4kcal) of "
+                                + (carbs * 4) + " larger than total calories of "
+                                + calories;
+                        Snackbar.make(findViewById(R.id.new_food),
+                                errMsg, Snackbar.LENGTH_LONG).show();
+                        setResult(RESULT_CANCELED, replyIntent);
+                    }
 
                     replyIntent.putExtra(EXTRA_REPLY_NAME, foodName);
                     replyIntent.putExtra(EXTRA_REPLY_FAVORITE, favorite);
