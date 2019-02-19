f964f03d956b6fbd55446a59403d5047bf831fd8
UlricusR <ulrich.rueth@gmail.com>
2019-02-19 23:47:28 +0100

    Started editing food via swiping

---
+++ app/src/main/java/info/rueth/fpucalculator/SwipeController.java
@@ -0,0 +1,26 @@
+package info.rueth.fpucalculator;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+import static android.support.v7.widget.helper.ItemTouchHelper.*;
+
+public class SwipeController extends ItemTouchHelper.Callback {
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        return makeMovementFlags(0, LEFT | RIGHT);
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView,
+                          RecyclerView.ViewHolder viewHolder,
+                          RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+    }
+}
