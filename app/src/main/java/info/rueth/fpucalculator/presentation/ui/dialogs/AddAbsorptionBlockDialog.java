246eca3fd7a77756f7bf7f78d52862c6ec835103
UlricusR <ulrich.rueth@gmail.com>
2019-03-25 22:29:27 +0100

    Refactored - App to AppCompat and removed unused resources

--- app/src/main/java/info/rueth/fpucalculator/presentation/ui/dialogs/AddAbsorptionBlockDialog.java
+++ app/src/main/java/info/rueth/fpucalculator/presentation/ui/dialogs/AddAbsorptionBlockDialog.java
@@ -1,10 +1,10 @@
 package info.rueth.fpucalculator.presentation.ui.dialogs;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -44,6 +44,7 @@ public class AddAbsorptionBlockDialog extends AppCompatDialogFragment {
     }
 
     @Override
+    @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Re-create FPU and absorption time values in case screen has been rotated
         if (savedInstanceState != null && savedInstanceState.containsKey(FPU_KEY)) {
