fcb8d52a78a3144fac18e45ff814a20f7477ed97
UlricusR <ulrich.rueth@gmail.com>
2019-03-28 22:03:44 +0100

    Went back to (deprecated) PreferenceScreen - new androidx not yet working

--- app/src/main/java/info/rueth/fpucalculator/presentation/ui/PreferencesActivity.java
+++ app/src/main/java/info/rueth/fpucalculator/presentation/ui/PreferencesActivity.java
@@ -1,20 +1,15 @@
 package info.rueth.fpucalculator.presentation.ui;
 
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
 import info.rueth.fpucalculator.R;
 
-public class PreferencesActivity extends AppCompatActivity {
+public class PreferencesActivity extends PreferenceActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.preferences_container, new PreferencesFragment())
-                .commit();
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
