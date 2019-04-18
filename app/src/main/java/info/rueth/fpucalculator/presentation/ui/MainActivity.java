708753050a685a67cb057e11043498d1ec757ff0
UlricusR <ulrich.rueth@gmail.com>
2019-04-18 23:30:40 +0200

    Moved Database maintenance to Settings

--- app/src/main/java/info/rueth/fpucalculator/presentation/ui/MainActivity.java
+++ app/src/main/java/info/rueth/fpucalculator/presentation/ui/MainActivity.java
@@ -2,7 +2,6 @@ package info.rueth.fpucalculator.presentation.ui;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
@@ -11,18 +10,15 @@ import android.view.View;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import info.rueth.fpucalculator.R;
-import info.rueth.fpucalculator.domain.repository.DatabaseJsonExportService;
-import info.rueth.fpucalculator.domain.repository.DatabaseJsonImportService;
-import info.rueth.fpucalculator.domain.repository.DatabaseRawExportService;
 import info.rueth.fpucalculator.domain.repository.FoodDataRepository;
 import info.rueth.fpucalculator.presentation.adapter.FoodListAdapter;
 
@@ -159,24 +155,6 @@ public class MainActivity extends AppCompatActivity {
                 // Start preferences activity
                 startActivity(new Intent(this, PreferencesActivity.class));
                 return true;
-            case R.id.action_exportdb_raw:
-                // Export database
-                Intent rawFileIntent = selectFileForExport("fpu_calculator_database", "*/*");
-                // Start activity
-                startActivityForResult(rawFileIntent, SAF_CREATE_RAW_EXPORT_FILE);
-                return true;
-            case R.id.action_exportdb_json:
-                // Export database
-                Intent jsonFileExportIntent = selectFileForExport("fpu_calculator_database.json", "application/json");
-                // Start activity
-                startActivityForResult(jsonFileExportIntent, SAF_CREATE_JSON_EXPORT_FILE);
-                return true;
-            case R.id.action_importdb_json:
-                // Import database
-                Intent jsonFileImportIntent = selectFileForImport("application/json");
-                // Start activity
-                startActivityForResult(jsonFileImportIntent, SAF_CREATE_JSON_IMPORT_FILE);
-                return true;
             case R.id.action_editabsorptionscheme:
                 // Edit absorption scheme
                 startActivity(new Intent(this, AbsorptionSchemeActivity.class));
@@ -188,112 +166,6 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);            
         }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK && data != null && requestCode == SAF_CREATE_RAW_EXPORT_FILE) {
-            // Path to file (as Content Provider URI)
-            Uri fileUri = data.getData();
-            exportRawDatabase(fileUri);
-        } else if (resultCode == RESULT_OK && data != null && requestCode == SAF_CREATE_JSON_EXPORT_FILE) {
-            // Path to file (as Content Provider URI)
-            Uri fileUri = data.getData();
-            exportJsonDatabase(fileUri);
-        } else if (resultCode == RESULT_OK && data != null && requestCode == SAF_CREATE_JSON_IMPORT_FILE) {
-            // Path to file (as Content Provider URI)
-            Uri fileUri = data.getData();
-            importJsonDatabase(fileUri);
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    private Intent selectFileForExport(String fileDefaultName, String mimeType) {
-        // Activity to select file for export
-        Intent fileIntent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-
-        // Category to be able to open file
-        fileIntent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        // Set mime type
-        fileIntent.setType(mimeType);
-
-        // Proposed name of file
-        fileIntent.putExtra(Intent.EXTRA_TITLE, fileDefaultName);
-
-        return fileIntent;
-    }
-
-    private Intent selectFileForImport(String mimeType) {
-        // Activity to select file for import
-        Intent fileIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-
-        // Category to be able to open file
-        fileIntent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        // Set mime type
-        fileIntent.setType("*/*"); // Unfortunately no way to specify *.json in Storage Access Framework :-(
-
-        return fileIntent;
-    }
-
-    private void exportRawDatabase(final Uri fileUri) {
-        // Initialize export service
-        Intent exportService = new Intent(this, DatabaseRawExportService.class);
-
-        // Set export file uri to service
-        exportService.setData(fileUri);
-
-        // Start service
-        startService(exportService);
-    }
-
-    private void exportJsonDatabase(final Uri fileUri) {
-        // Initialize export service
-        Intent exportService = new Intent(this, DatabaseJsonExportService.class);
-
-        // Set export file uri to service
-        exportService.setData(fileUri);
-
-        // Start service
-        startService(exportService);
-    }
-
-    private void importJsonDatabase(final Uri fileUri) {
-        // Initialize import service
-        Intent importService = new Intent(this, DatabaseJsonImportService.class);
-
-        // Set import file uri to service
-        importService.setData(fileUri);
-
-        // Ask user for replace or append mode
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-
-        // Set title and message
-        alert.setTitle(R.string.dbimport_dialog_mode_title);
-        alert.setMessage(R.string.dbimport_dialog_mode_message);
-
-        // Set positive button = append
-        alert.setPositiveButton(R.string.dbimport_dialog_mode_button_append, (dialog, which) -> {
-            // Set append mode to true (means to append the data)
-            importService.putExtra(SERVICE_IMPORTJSON, true);
-
-            // Start service
-            startService(importService);
-        });
-
-        // Set negative button = replace
-        alert.setNegativeButton(R.string.dbimport_dialog_mode_button_replace, (dialog, which) -> {
-            // Set append mode to false (means replace the data)
-            importService.putExtra(SERVICE_IMPORTJSON, false);
-
-            // Start service
-            startService(importService);
-        });
-
-        // Show
-        alert.create().show();
     }
 
     private void showAboutDialog() {
--- app/src/main/java/info/rueth/fpucalculator/presentation/ui/PreferencesFragment.java
+++ app/src/main/java/info/rueth/fpucalculator/presentation/ui/PreferencesFragment.java
@@ -1,9 +1,13 @@
 package info.rueth.fpucalculator.presentation.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -12,9 +16,19 @@ import java.util.regex.Pattern;
 
 import info.rueth.fpucalculator.R;
 import info.rueth.fpucalculator.domain.repository.AbsorptionSchemeRepository;
+import info.rueth.fpucalculator.domain.repository.DatabaseJsonExportService;
+import info.rueth.fpucalculator.domain.repository.DatabaseJsonImportService;
+import info.rueth.fpucalculator.domain.repository.DatabaseRawExportService;
+
+import static android.app.Activity.RESULT_OK;
 
 public class PreferencesFragment extends PreferenceFragmentCompat {
     private static final String LOG_TAG = "PreferencesFragment";
+    private static final int SAF_CREATE_RAW_EXPORT_FILE = 200;
+    private static final int SAF_CREATE_JSON_EXPORT_FILE = 300;
+    private static final int SAF_CREATE_JSON_IMPORT_FILE = 400;
+
+    private static final String SERVICE_IMPORTJSON = "info.rueth.fpucalculator.ImportJson";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -33,6 +47,140 @@ public class PreferencesFragment extends PreferenceFragmentCompat {
             Log.e(LOG_TAG, e.getLocalizedMessage());
             Toast.makeText(getContext(), R.string.err_cannotcreatepreferences + ": " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
         }
+
+        // Get the database maintenance preferences and set onClickListener
+        Preference dbExportJsonPref = findPreference(getString(R.string.preference_dbexport_json_key));
+        dbExportJsonPref.setOnPreferenceClickListener(preference -> {
+            // Export database
+            Intent jsonFileExportIntent = selectFileForExport("fpu_calculator_database.json", "application/json");
+            // Start activity
+            startActivityForResult(jsonFileExportIntent, SAF_CREATE_JSON_EXPORT_FILE);
+            return true;
+        });
+
+        Preference dbImportJsonPref = findPreference(getString(R.string.preference_dbimport_json_key));
+        dbImportJsonPref.setOnPreferenceClickListener(preference -> {
+            // Import database
+            Intent jsonFileImportIntent = selectFileForImport("application/json");
+            // Start activity
+            startActivityForResult(jsonFileImportIntent, SAF_CREATE_JSON_IMPORT_FILE);
+            return true;
+        });
+
+        Preference dbExportRawPref = findPreference(getString(R.string.preference_dbexport_raw_key));
+        dbExportRawPref.setOnPreferenceClickListener(preference -> {
+            // Export database
+            Intent rawFileIntent = selectFileForExport("fpu_calculator_database", "*/*");
+            // Start activity
+            startActivityForResult(rawFileIntent, SAF_CREATE_RAW_EXPORT_FILE);
+            return true;
+        });
+    }
+
+    private Intent selectFileForExport(String fileDefaultName, String mimeType) {
+        // Activity to select file for export
+        Intent fileIntent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+
+        // Category to be able to open file
+        fileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Set mime type
+        fileIntent.setType(mimeType);
+
+        // Proposed name of file
+        fileIntent.putExtra(Intent.EXTRA_TITLE, fileDefaultName);
+
+        return fileIntent;
+    }
+
+    private Intent selectFileForImport(String mimeType) {
+        // Activity to select file for import
+        Intent fileIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Category to be able to open file
+        fileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Set mime type
+        fileIntent.setType("*/*"); // Unfortunately no way to specify *.json in Storage Access Framework :-(
+
+        return fileIntent;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && data != null && requestCode == SAF_CREATE_RAW_EXPORT_FILE) {
+            // Path to file (as Content Provider URI)
+            Uri fileUri = data.getData();
+            exportRawDatabase(fileUri);
+        } else if (resultCode == RESULT_OK && data != null && requestCode == SAF_CREATE_JSON_EXPORT_FILE) {
+            // Path to file (as Content Provider URI)
+            Uri fileUri = data.getData();
+            exportJsonDatabase(fileUri);
+        } else if (resultCode == RESULT_OK && data != null && requestCode == SAF_CREATE_JSON_IMPORT_FILE) {
+            // Path to file (as Content Provider URI)
+            Uri fileUri = data.getData();
+            importJsonDatabase(fileUri);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void exportRawDatabase(final Uri fileUri) {
+        // Initialize export service
+        Intent exportService = new Intent(getContext(), DatabaseRawExportService.class);
+
+        // Set export file uri to service
+        exportService.setData(fileUri);
+
+        // Start service
+        getActivity().startService(exportService);
+    }
+
+    private void exportJsonDatabase(final Uri fileUri) {
+        // Initialize export service
+        Intent exportService = new Intent(getContext(), DatabaseJsonExportService.class);
+
+        // Set export file uri to service
+        exportService.setData(fileUri);
+
+        // Start service
+        getActivity().startService(exportService);
+    }
+
+    private void importJsonDatabase(final Uri fileUri) {
+        // Initialize import service
+        Intent importService = new Intent(getContext(), DatabaseJsonImportService.class);
+
+        // Set import file uri to service
+        importService.setData(fileUri);
+
+        // Ask user for replace or append mode
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+
+        // Set title and message
+        alert.setTitle(R.string.dbimport_dialog_mode_title);
+        alert.setMessage(R.string.dbimport_dialog_mode_message);
+
+        // Set positive button = append
+        alert.setPositiveButton(R.string.dbimport_dialog_mode_button_append, (dialog, which) -> {
+            // Set append mode to true (means to append the data)
+            importService.putExtra(SERVICE_IMPORTJSON, true);
+
+            // Start service
+            getActivity().startService(importService);
+        });
+
+        // Set negative button = replace
+        alert.setNegativeButton(R.string.dbimport_dialog_mode_button_replace, (dialog, which) -> {
+            // Set append mode to false (means replace the data)
+            importService.putExtra(SERVICE_IMPORTJSON, false);
+
+            // Start service
+            getActivity().startService(importService);
+        });
+
+        // Show
+        alert.create().show();
     }
 
     private class NonNegativeIntegerChecker implements Preference.OnPreferenceChangeListener {
