86f15d97bc2fa6b42168a1e7bbcb68edb1b2449d
Ulrich R <UlricusR@users.noreply.github.com>
2019-04-22 01:07:54 +0200

    Added filtering after altering the repository data

--- app/src/main/java/info/rueth/fpucalculator/presentation/ui/MainActivity.java
+++ app/src/main/java/info/rueth/fpucalculator/presentation/ui/MainActivity.java
@@ -87,7 +87,12 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Attach data observer, update cached copy of the food items in the adapter
-        FoodDataRepository.getInstance(getApplication()).getAllFoodVM().observe(this, foodViewModels -> adapter.setAllFood(foodViewModels, favoriteButton.isChecked()));
+        FoodDataRepository.getInstance(getApplication()).getAllFoodVM().observe(this, foodViewModels -> {
+            adapter.setAllFood(foodViewModels, favoriteButton.isChecked()));
+            
+            // Add filter if already initialized
+            if (searchView != null) onQueryTextChange(searchView.getQuery().toString());
+        }
 
         // New food and meal calc FABs
         fabAdd = findViewById(R.id.fab_new);
